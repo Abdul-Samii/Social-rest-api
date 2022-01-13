a9f02b3007ce6d5f440de15dfc9aaa81c1578c6d
Abdul-Samii <ksamk100474@gmail.com>
2022-01-14 01:06:09 +0500

    Login API

--- routes/auth.js
+++ routes/auth.js
@@ -23,4 +23,22 @@ router.post('/register', async (req,res)=>{
     
 });
 
+
+//login
+router.post('/login',async(req,res)=>{
+    try{
+        const user = await User.findOne({email:req.body.email});
+        !user && res.status(404).json("User not Found");
+
+        const validPassword = await bcrypt.compare(req.body.password,user.password);
+        !validPassword && res.status(400).json("Wrong Password");
+
+        res.status(200).json(user);
+    }
+    catch(e)
+    {
+        console.log(e);
+    }
+})
+
 module.exports = router;
